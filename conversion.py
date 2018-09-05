5603855de82964d91c2e70a4119833b688a1b7ff
Dale Weigt <D.M.Weigt@soton.ac.uk>
2018-09-05 11:39:24 +0100

    Always add documentation you fool

--- conversion.py
+++ conversion.py
@@ -1,10 +1,19 @@
 def kelvin_to_celsius(temp):
+	"""
+	Covert a temperture from Kelvin to Celsius
+	"""
 	return temp - 273.15
 
 def celsius_to_fahr(temp):
+	"""
+	Covert a temp. from Celsius to Fahrenheit
+	"""
 	return temp * (9/5) + 32
 
 def kelvin_to_fahr(temp):
+	"""
+	Covert a temp. from Kelvin to Fahrenheit
+	"""
 	temp_c = kelvin_to_celsius(temp)
 	result = celsius_to_fahr(temp_c)
 	return result
